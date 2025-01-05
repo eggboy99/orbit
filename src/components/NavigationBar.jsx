0e658b7a2f815eae057a542730fa846f9143f65f
eggboy99 <shayne.weiliang@gmail.com>
2025-01-05 13:46:49 +0800

    Created required directories and global CSS variables

---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,14 @@
+const NavigationBar = () => {
+  return (
+    <>
+      <ul>
+        <li>Explore</li>
+        <li>Community</li>
+        <li>Social Feed</li>
+        <li>Articles</li>
+      </ul>
+    </>
+  );
+};
+
+export default NavigationBar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import NavigationBar from "../components/NavigationBar";
+const Home = () => {
+  return (
+    <div>
+      <nav>
+        <NavigationBar />
+      </nav>
+    </div>
+  );
+};
+
+export default Home;
